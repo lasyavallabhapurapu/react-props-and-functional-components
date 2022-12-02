894063b364bdcdbde77ad29b5875c91089911041
unique-king <lingamguntaleelavamsi4668@gmail.com>
2022-12-02 11:52:03 +0530

    Events

--- src/App.js
+++ src/App.js
@@ -1,27 +1,7 @@
-/*import ClassComponent from "./ClassComponent"
-
-function Demo(){
-  return(
-    <section>
-    <img src="https://wallpapercave.com/wp/pjBP3eN.jpg" alt="nature" width="50%"/>
-    <p>Hello fellows</p>
-    <h1>This is h1 tag</h1>
-    <ClassComponent/>
-    </section>
-    )
+import React from 'react'
+import Events from './Events'
+export default function App() {
+  return (
+    <div><Events/></div>
+  )
 }
-export default Demo*/
-//import FunctionPropExample from './FunctionPropExample'
-import StateExample from './StateExample'
-function App(){
-  return(
-    <div>
-    <h1>This is about props example</h1>
-    {/*PropsExample name="las" age="18"/>
-    {PropsExample name="akhi" age="16"/>
-  FunctionPropsExample framework="react" number="23"/>*/}
-  <StateExample/>
-    </div>
-    )
-}
-export default App 
